f29b80a02db5931d99dbd5567c9f00b2074162e8
Andrew Buhler <terminalvelocity13@gmail.com>
2024-07-14 16:29:10 -0400

    Update 1
    - Added user input.

--- src/NumberGuessingGame/NumberGuessingGame.java
+++ src/NumberGuessingGame/NumberGuessingGame.java
@@ -1,5 +1,7 @@
 package NumberGuessingGame;
 
+import java.util.Scanner;
+
 // Game where a player guesses a number that the computer picks. 
 public class NumberGuessingGame {
 
@@ -8,7 +10,16 @@ public class NumberGuessingGame {
 	} // constructor
 
 	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
 
+		System.out.print("Please guess a number: ");
+
+		if (input.hasNextInt()) {
+			int guess = input.nextInt();
+			System.out.println(guess);
+		} else {
+			System.out.println("Not a correct guess. Try again.");
+		} // if
 	} // main
 
 } // class
