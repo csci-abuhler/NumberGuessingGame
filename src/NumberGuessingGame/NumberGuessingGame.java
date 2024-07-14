d7766a09da6668190d36bedc46aef4c5d79bbfc8
Andrew Buhler <terminalvelocity13@gmail.com>
2024-07-14 17:01:13 -0400

    Update 2
    - Game is functional

--- src/NumberGuessingGame/NumberGuessingGame.java
+++ src/NumberGuessingGame/NumberGuessingGame.java
@@ -1,25 +1,61 @@
 package NumberGuessingGame;
 
+import java.util.Random;
 import java.util.Scanner;
 
 // Game where a player guesses a number that the computer picks. 
 public class NumberGuessingGame {
 
-	public NumberGuessingGame() {
-
+	private static final int guesses = 3;
+	private static int chances = guesses;
+	private static int playerGuess = -1;
+	private static int computerSelection = -1;
+	private static boolean testingActive = true;
+	private static Scanner input = new Scanner(System.in);
+	
+	public NumberGuessingGame(int range) {
+		Random computerPicker = new Random();
+		computerSelection = computerPicker.nextInt(range);
+		if (testingActive) {
+			System.out.println("Number picked is: " + computerSelection);
+		} // if
 	} // constructor
 
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
+		// Computer number is picked.
+		do {
+			System.out.print("Please pick a positive number for the maximum value: ");
+			int pick = input.nextInt();
 
-		System.out.print("Please guess a number: ");
-
-		if (input.hasNextInt()) {
-			int guess = input.nextInt();
-			System.out.println(guess);
-		} else {
-			System.out.println("Not a correct guess. Try again.");
-		} // if
+			if (pick > -1) {
+				NumberGuessingGame ngg = new NumberGuessingGame(pick);
+				break;
+			} else {
+				System.out.println("Not a correct guess. Try again.");
+			} // if
+		} while (true);
+		
+		// Player input is checked.
+		do {
+			System.out.print("Please guess a number: ");
+			if (input.hasNextInt()) {
+				playerGuess = input.nextInt();
+			} else {
+				System.out.println("Not a correct guess. Try again.");
+				continue;
+			} // if
+		
+			if (playerGuess == computerSelection) {
+				System.out.println("YOU WIN!");
+				break;
+			} else {
+				chances--;
+				if (chances == 0) {
+					System.out.println("YOU HAVE LOST!");
+					break;
+				} // if
+				System.out.println("Try again...");
+			} // if
+		} while (chances > 0);
 	} // main
-
 } // class
